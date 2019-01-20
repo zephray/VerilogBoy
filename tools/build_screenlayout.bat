fb76224b5f4283a12c0909435c5809a246b7d91f
Wenting Zhang <zephray@outlook.com>
2019-01-20 19:07:22 +0000

    tools: update bin2mif build

--- tools/build_screenlayout.bat
+++
@@ -1,6 +0,0 @@
-cd screenlayout
-..\bin2mif\bin2mif screen1.txt screen1.mif 80
-..\bin2mif\bin2mif screen2.txt screen2.mif 80
-copy screen1.mif ..\..\rtl\GameBoy\screen1.mif
-copy screen2.mif ..\..\rtl\GameBoy\screen2.mif
-pause
