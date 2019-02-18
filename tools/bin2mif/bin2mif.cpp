0ee7fe9cfeaa619efee7324e4d3256d6a8b21de3
Wenting Zhang <zephray@outlook.com>
2019-02-17 20:06:32 -0500

    tools/bin2mif: add bit width option

--- tools/bin2mif/bin2mif.cpp
+++ tools/bin2mif/bin2mif.cpp
@@ -10,26 +10,32 @@ using namespace std;
 int main(int argc, char * argv[])
 {
 	int size;
-	if (argc == 4) {
+	int width = 8;
+	if (argc == 5) {
+		width = atoi(argv[4]);
+	}
+	if (argc >= 4) {
 		size = atoi(argv[3]);
 	}
 	else if (argc == 3) {
 		size = 256; // Default GB BROM Size
 	}
 	else {
-		cout << "Usage: bin2mif <input.bin> <output.mif> [size]" << endl;
+		cout << "Usage: bin2mif <input.bin> <output.mif> [size] [width]" << endl;
 		return -1;
 	}
-	
+
 	ifstream inFile(argv[1], ios::in | ios::binary);
 	ofstream outFile(argv[2], ios::out | ios::trunc);
-	
+
 	char * buffer;
 
 	buffer = (char *)malloc(size);
 	inFile.read(buffer, size);
 	for (int i = 0; i < size; i++) {
-		outFile << hex << uppercase << setw(2) << setfill('0') << int((uint8_t)buffer[i]) << endl;
+		outFile << hex << uppercase << setw(2) << setfill('0') << int((uint8_t)buffer[i]);
+		if (i % (width / 8) == (width / 8 - 1))
+			outFile << endl;
 	}
 
 	inFile.close();
